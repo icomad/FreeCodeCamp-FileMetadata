53c249b38bbff630a38c8932ce23742ba8cf0a7f
Federico Madaffari <icomad@me.com>
2018-11-06 01:38:37 +0100

    api route updated

--- server.js
+++ server.js
@@ -2,8 +2,11 @@
 
 var express = require('express');
 var cors = require('cors');
+var multer = require('multer');
 
 // require and use "multer"...
+var storage = multer.memoryStorage();
+var upload = multer({ storage });
 
 var app = express();
 
@@ -11,11 +14,15 @@ app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
-     res.sendFile(process.cwd() + '/views/index.html');
-  });
+  res.sendFile(process.cwd() + '/views/index.html');
+});
 
-app.get('/hello', function(req, res){
-  res.json({greetings: "Hello, API"});
+app.post('/api/fileanalyse', upload.single('upfile'), function (req, res) {
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  });
 });
 
 app.listen(process.env.PORT || 3000, function () {
